beb00d1916f16621d7f638e4d214db8b362aad00
Patrick Altaie <patrick.altaie@iconsolutions.com>
2018-05-25 17:07:46 +0100

    Remove solved from master

--- src/test/java/com/example/calculator/MyStepdefs.java
+++ src/test/java/com/example/calculator/MyStepdefs.java
@@ -1,7 +1,6 @@
 package com.example.calculator;
 
 import com.jayway.jsonpath.JsonPath;
-import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -12,8 +11,6 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -33,9 +30,6 @@ public class MyStepdefs {
     private int secondNumber;
     private int singleResult;
 
-    private RequestBody batchRequest;
-    private List<Integer> resultList;
-
     @Given("^I have two numbers that are (\\d+) and (\\d+)$")
     public void iHaveTwoNumbersThatAreAnd(int firstNumber, int secondNumber) throws Throwable {
         this.firstNumber = firstNumber;
@@ -54,37 +48,8 @@ public class MyStepdefs {
         singleResult = JsonPath.read(response.body().string(), "$.result");
     }
 
-    @When("^I ask the service to '(\\w+)' them in batch$")
-    public void iAskTheServiceToAddThemInBatch(String operation) throws Throwable {
-        Request request = new Request.Builder()
-                .url(baseUrl + "/batch/" + operation)
-                .post(batchRequest)
-                .build();
-        Response response = okHttpClient.newCall(request).execute();
-        assertNotNull("Response body was null!", response.body());
-        String x = response.body().string();
-        resultList = JsonPath.read(x, "$..result");
-    }
-
     @Then("^the result should be (-?\\d+)$")
     public void theResultShouldBe(int expectedResult) throws Throwable {
         assertEquals(expectedResult, singleResult);
-    }
-
-    @Given("^I have the following pairs of numbers to (add|subtract|divide|multiply):")
-    public void iHaveTheFollowingSetOfNumbersToAddTogether(String operation, DataTable dataTable) throws Throwable {
-        StringBuilder payload = new StringBuilder("[");
-        dataTable.getGherkinRows().forEach(row -> {
-            payload.append(String.format("{\"firstNumber\":%s,\"secondNumber\":%s},", row.getCells().get(0), row.getCells().get(1)));
-        });
-        payload.append("]");
-        batchRequest = RequestBody.create(JSON, payload.toString().replaceAll(",]", "]"));
-    }
-
-    @Then("^the results should be:$")
-    public void theResultsShouldBe(List<Integer> expectedResults) throws Throwable {
-        for (int i = 0; i < expectedResults.size(); i++) {
-            assertEquals(expectedResults.get(i), resultList.get(i));
-        }
     }
 }
--- src/test/resources/features/Add.feature
+++ src/test/resources/features/Add.feature
@@ -4,24 +4,3 @@ Feature: Addition
     Given I have two numbers that are 3 and 2
     When I ask the service to 'add' them
     Then the result should be 5
-
-  Scenario Outline: A few examples of addition
-    Given I have two numbers that are <first number> and <second number>
-    When I ask the service to 'add' them
-    Then the result should be <the result>
-    Examples:
-      | first number | second number | the result |
-      | 3            | 2             | 5          |
-      | 4            | 9             | 13         |
-      | 1000         | 2000          | 3000       |
-
-  Scenario: Adding in batch
-    Given I have the following pairs of numbers to add:
-      | 4 | 4 |
-      | 5 | 6 |
-      | 6 | 7 |
-    When I ask the service to 'add' them in batch
-    Then the results should be:
-      | 8  |
-      | 11 |
-      | 13 |
